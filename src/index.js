445d6e68c221a1002f41ab854ba1d9b87e4655c6
Ágatha <agathamagnusg@hotmail.com>
2024-06-06 20:04:05 -0300

    🐛 fix: fix routes

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import Router from "./routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
